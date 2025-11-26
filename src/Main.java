bf180662417158ca0a970c3ce0c575d4594ca16c
bhagyarajsinh jadeja <jadejabhagyarajsinh9245@gmail.com>
2025-11-26 09:28:31 +0530

    added mul and div

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,11 @@ public class Main {
 
         int add = n1+n2;
         int sub = n1-n2;
-
+        int mul = n1*n2;
+        int div = n1/n2;
         System.out.println("Addition: "+ add);
         System.out.println("Subtraction : "+ sub);
+        System.out.println("Multiplication: "+ mul);
+        System.out.println("Division : "+ div);
     }
 }
