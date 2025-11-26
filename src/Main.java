98b0d8ee7de947cbac4c07ba700df956c42fa999
bhagyarajsinh jadeja <jadejabhagyarajsinh9245@gmail.com>
2025-11-26 09:18:24 +0530

    added Main with performing addition and subtraction

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        System.out.println("Enter num1: ");
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+
+        System.out.println("Enter num2: ");
+        int n2 = sc.nextInt();
+
+        int add = n1+n2;
+        int sub = n1-n2;
+
+        System.out.println("Addition: "+ add);
+        System.out.println("Subtraction : "+ sub);
+    }
+}
